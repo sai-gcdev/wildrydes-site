2a96c28949bae5df7a4232da55063b671e5ac27b
Sai <sai.gcdev@gmail.com>
2025-08-02 16:45:35 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,12 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_idRT4MdsP', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'lgagkmse18r01aiv27126fhkp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: 'tb0jt9jhm24tf0j584oqs2fe1sdi4hpilh1bvdcicv1cqrgfb8t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
